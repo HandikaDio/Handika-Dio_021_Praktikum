54aa8ec116b4ae9ed5aa000890afd3ab67226111
HandikaDio <90175137+HandikaDio@users.noreply.github.com>
2021-10-25 19:38:26 +0700

    Add files via upload

---
+++ suku pertama.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+	
+	cout<<" Menghitung Jumlah 6 Suku Pertama"<<endl;
+
+
+	int jmldr,i;
+
+	cout<<"Jumlah Deret Yang Di Inginkan: ";
+	cin>>jmldr;
+
+	cout<<"--------------------------------------------"<<endl;
+	for(i=1; i<=jmldr;i++) {
+		cout<<i*i*i <<" ";
+	}
+	cout<<endl;
+	return 0;
+}
